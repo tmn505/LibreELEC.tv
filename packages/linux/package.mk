eb4acd4a1540d027f9d45ac6eac6fb5b2712ac9d
Tomasz Maciej Nowak <tmn505@gmail.com>
2026-04-20 15:47:29 +0200

    Revert "linux: update to 6.19"
    This reverts commit 784183c2aa7a9ae2b35fbdfad4f034905aab4047.

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -37,8 +37,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="default rockchip"
     ;;
   *)
-    PKG_VERSION="6.19"
-    PKG_SHA256="303079a8250b8f381f82b03f90463d12ac98d4f6b149b761ea75af1323521357"
+    PKG_VERSION="6.18.8"
+    PKG_SHA256="37f0c5d5c242c1d604e87d48f08795e861a5a85f725b4ca11d0a538f12ff8cff"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     case ${DEVICE} in
